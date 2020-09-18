1761bc6c3b91354c25440eef965a2a537ad6fca8
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-09-18 10:45:47 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var nama = "Tri Afriliyanti"
+var statusPerkawinan  = true
+var a = null
+var b
+var AllKarawang = ["Tasya", "Maulana", "Iqbal", "Rahmat"]
+
+console.log(typeof nama)
+console.log(typeof statusPerkawinan)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof AllKarawang)
+
+console.log(nama)
+console.log(statusPerkawinan)
+console.log(a)
+console.log(b)
+console.log(AllKarawang)
